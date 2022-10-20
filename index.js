eaf7cee8b7d5059986608803df86d065d381c416
hunhavoc <hun.havoc@gmail.com>
2022-10-20 10:38:08 +0200

    chore: init app

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve('pages/index.html'));
 });
 
-app.get('/hello', (req, res) => {
-  debugger;
-  res.sendFile(path.resolve('pages/index.html'));
-});
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
